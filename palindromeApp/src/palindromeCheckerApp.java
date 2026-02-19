e57d68629257d060f0ea115cfea7c74c367785f4
sneha434 <snehasinghanvesha@gmail.com>
2026-02-19 10:46:36 +0530

    UC1 solved

---
+++ palindromeApp/src/palindromeCheckerApp.java
@@ -0,0 +1,8 @@
+public class palindromeCheckerApp {
+    static void main() {
+        System.out.println("Welcome to the palindrome checker management system");
+        System.out.println("version : 1.0");
+        System.out.println("System initialized  git branchsuccessfully");
+
+    }
+}

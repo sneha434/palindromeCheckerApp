678a01d3e992a1c19f5bc8af7ddaba0e1df19e23
sneha434 <snehasinghanvesha@gmail.com>
2026-02-19 11:13:06 +0530

    UC2 completed

---
+++ palindromeApp/src/PalindromeStringBoolean.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PalindromeStringBoolean {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        boolean isPalindrome = true;
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("It is a palindrome.");
+        } else {
+            System.out.println("It is not a palindrome.");
+        }
+
+        sc.close();
+    }
+}
